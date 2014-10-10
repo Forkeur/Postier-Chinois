58184edc171a6f5a34798df758da2aa0fcdc13f3
maxisoft <maxisoft.eu@gmail.com>
2014-10-10 10:15:19 +0200

    init repo

---
+++ main.c
@@ -0,0 +1,17 @@
+/* 
+ * File:   main.c
+ * Author: maxime
+ *
+ * Created on 10 octobre 2014, 10:04
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
